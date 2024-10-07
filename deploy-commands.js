2c6615e8e9713f5bf09845e84608f9a03c8a02a6
Yixuan_Wang <newbietedsan@gmail.com>
2024-10-06 20:37:27 -0700

    Enable command prompt operation

--- deploy-commands.js
+++ deploy-commands.js
@@ -2,6 +2,23 @@ const { REST, Routes } = require('discord.js');
 const { clientId, guildId, token } = require('./config.json');
 const fs = require('node:fs');
 const path = require('node:path');
+const { argv } = require('node:process');
+
+const args = {"-d": ["guild", "global"]}
+
+if (argv[2] != null) {
+  if (!args.hasOwnProperty(argv[2])) {
+    console.log("Usage: node deploy-commands.js <command> <option>\n\t" +
+      "There is no such command: " + argv[2] + (argv[3] == null ?  "" : " " + argv[3]));
+    return;
+  } else if (!args[argv[2]].includes(argv[3])) {
+    console.log("Usage: node deploy-commands.js <command> <option>\n\t" +
+      "There is no such command: " + argv[2] + (argv[3] == null ?  "" : " " + argv[3]));
+    return;
+  }
+}
+
+
 
 const commands = [];
 // Grab all the command folders from the commands directory you created earlier
@@ -27,11 +44,20 @@ for (const folder of commandFolders) {
 // Construct and prepare an instance of the REST module
 const rest = new REST().setToken(token);
 
-
-// // for guild-based commands
-// rest.put(Routes.applicationGuildCommands(clientId, guildId), { body: [] })
-// 	.then(() => console.log('Successfully deleted all guild commands.'))
-// 	.catch(console.error);
+if (argv[2] == "-d") {
+  if (argv[3] == "guild") {
+    // for guild-based commands
+    rest.put(Routes.applicationGuildCommands(clientId, guildId), { body: [] })
+        .then(() => console.log('Successfully deleted all guild commands.'))
+        .catch(console.error);
+  } else if (argv[3] == "global") {
+    // for global commands
+    rest.put(Routes.applicationCommands(clientId), { body: [] })
+        .then(() => console.log('Successfully deleted all application commands.'))
+        .catch(console.error);
+  }
+  return;
+}
 
 // and deploy your commands!
 (async () => {
@@ -40,12 +66,16 @@ const rest = new REST().setToken(token);
 
 		// The put method is used to fully refresh all commands in the guild with the current set
 
-    // for guild-based commands
+    // // for global commands
+		// const data = await rest.put(
+		// 	Routes.applicationCommands(clientId),
+	  //   { body: commands },
+		// );
 
-
+    // The put method is used to fully refresh all commands in the guild with the current set
 		const data = await rest.put(
-			Routes.applicationCommands(clientId),
-	    { body: commands },
+			Routes.applicationGuildCommands(clientId, guildId),
+			{ body: commands },
 		);
 
 		console.log(`Successfully reloaded ${data.length} application (/) commands.`);
