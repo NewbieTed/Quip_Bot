22af4f0d422a9e291e588dc0d695fc5972313d65
Yixuan_Wang <newbietedsan@gmail.com>
2024-10-07 19:42:05 -0700

    Restructure Files

---
+++ Back/src/main/java/com/quip/Quip_Bot/QuipBotApplication.java
@@ -0,0 +1,13 @@
+package com.quip.Quip_Bot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QuipBotApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(QuipBotApplication.class, args);
+	}
+
+}
---
+++ Back/src/test/java/com/quip/Quip_Bot/QuipBotApplicationTests.java
@@ -0,0 +1,13 @@
+package com.quip.Quip_Bot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class QuipBotApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
