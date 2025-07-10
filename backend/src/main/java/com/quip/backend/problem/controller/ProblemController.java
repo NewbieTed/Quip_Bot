31e58f8b42b52dc994a056f276d7cdc538c27e02
Yixuan_Wang <newbietedsan@gmail.com>
2025-07-10 15:25:46 +0800

    refactor: refactored add problem code

--- backend/src/main/java/com/quip/backend/problem/controller/ProblemController.java
+++ backend/src/main/java/com/quip/backend/problem/controller/ProblemController.java
@@ -2,14 +2,10 @@ package com.quip.backend.problem.controller;
 
 import com.quip.backend.dto.BaseResponse;
 import com.quip.backend.problem.dto.ProblemCreateDto;
-import com.quip.backend.problem.dto.ProblemDto;
 import com.quip.backend.problem.mapper.database.ProblemMapper;
-import com.quip.backend.problem.model.Problem;
-import com.quip.backend.problem.request.VerifyAnswerRequest;
 import com.quip.backend.problem.service.ProblemService;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
-import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
@@ -25,21 +21,7 @@ public class ProblemController {
 //        return BaseResponse.success("success", problemService.getProblem());
 //    }
 //
-//    @GetMapping("/verify")
-//    public BaseResponse<Boolean> verifyAnswer(@Valid @RequestBody VerifyAnswerRequest request) {
-//        if (request == null) {
-//            return BaseResponse.failure(HttpStatus.BAD_REQUEST.value(), "No data provided");
-//        }
-//
-//        long problemId = request.getProblemId();
-//        Problem problem = problemMapper.selectProblemById(problemId);
-//
-//        if (problem == null) {
-//            return BaseResponse.failure(HttpStatus.NOT_FOUND.value(),
-//                    "Problem with problem id " + problemId + " is not found");
-//        }
-//        return BaseResponse.success(problemService.verifyAnswer(problem, request.getAnswer()));
-//    }
+
 
     @PostMapping("/create")
     public BaseResponse<Boolean> addProblem(@Valid @RequestBody ProblemCreateDto problemCreateDto) {
